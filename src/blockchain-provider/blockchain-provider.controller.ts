a7538c573f1ef883f2b63cf3bb5f12f5593773c4
BlackH3art <jaroslaw.musielak@outlook.com>
2022-06-19 16:05:44 +0200

    fix: access control

--- src/blockchain-provider/blockchain-provider.controller.ts
+++ src/blockchain-provider/blockchain-provider.controller.ts
@@ -12,53 +12,53 @@ export class BlockchainProviderController {
     ) {}
     
     // get max whitelist spots
-    @Get('/whitelist/spots/max')
-    @Header('Access-Control-Allow-Origin', crooked)
+    // @Header('Access-Control-Allow-Origin', crooked)
+  @Get('/whitelist/spots/max')
   getMaxWhitelistSpots(): Promise<number> {
     return this.BlockchainProvider.getMaxWhitelistSpots();
   }
 
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/spots/max-public')
-  @Header('Access-Control-Allow-Origin', crooked)
   getMaxPublicWhitelistSpots(): Promise<number> {
     return this.BlockchainProvider.getMaxPublicWhitelistSpots();
   }
 
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/spots/max-owner')
-  @Header('Access-Control-Allow-Origin', crooked)
   getMaxOwnerWhitelistSpots(): Promise<number> {
     return this.BlockchainProvider.getMaxOwnerWhitelistSpots();
   }
 
   // get taken spots
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/spots/public-taken')
-  @Header('Access-Control-Allow-Origin', crooked)
   getPublicSpotsTaken(): Promise<number> {
     return this.BlockchainProvider.getPublicSpotsTaken();
   }
 
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/spots/owner-taken')
-  @Header('Access-Control-Allow-Origin', crooked)
   getOwnerSpotsTaken(): Promise<number> {
     return this.BlockchainProvider.getOwnerSpotsTaken();
   }
 
   // get whitelist open and close time
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/open')
-  @Header('Access-Control-Allow-Origin', crooked)
   getWhitelistOpen(): Promise<number> {
     return this.BlockchainProvider.getWhitelistOpen();
   }
 
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/close')
-  @Header('Access-Control-Allow-Origin', crooked)
   getWhitelistClose(): Promise<number> {
     return this.BlockchainProvider.getWhitelistClose();
   }
 
   // check if address is whitelisted
+  // @Header('Access-Control-Allow-Origin', crooked)
   @Get('/whitelist/isWhitelisted/:address')
-  @Header('Access-Control-Allow-Origin', crooked)
   getIsWhitelisted(
     @Param('address') address: string
   ): Promise<boolean> {
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://crookedsnouts.com',
-    // origin: 'http://localhost:3000'
-  });
+  // app.enableCors({
+  //   origin: 'https://crookedsnouts.com',
+  //   origin: 'http://localhost:3000'
+  // });
+
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3500);
 }
